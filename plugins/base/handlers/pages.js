a1405fc914e73697a63d9850bf4e628d6867edd5
Ekkapob <ekkapob@gmail.com>
2016-04-13 08:00:07 +0700

    products

--- plugins/base/handlers/pages.js
+++ plugins/base/handlers/pages.js
@@ -9,8 +9,29 @@ exports.index = (request, reply) => {
   reply.view('index');
 };
 
+var products = [
+  {
+    id: 'WPA-003',
+    car: 'HINO',
+    img: '/water_pump_assy/WPA-001.jpg',
+    name: 'KL (Long Shaft)',
+    details: 'MEGA, Super Turbo, EURO2',
+    partNo: '5-123-456-123',
+    engineModel: 'EH700'
+  },
+  {
+    id: 'WPA-003',
+    car: 'HINO',
+    img: '/water_pump_assy/WPA-001.jpg',
+    name: 'KL (Long Shaft)',
+    partNo: '5-123-456-123',
+  }
+];
+
 exports.products = (request, reply) => {
   // const key = request.yar.get('locale');
   // reply(key);
-  reply.view('products');
+  reply.view('products', {
+    products: products
+  });
 }
