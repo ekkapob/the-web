eaa7fac3eaf8b230224cbf67ed693ecedf986f1f
ek <ekkapob@gmail.com>
2016-08-02 01:47:44 +0700

    delete unused file

--- assets/js/mobile_search.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  var $mobileSearchForm = $('#mobile-search-form')
-  $mobileSearchForm.find('span').click(function(e){
-    e.preventDefault();
-    $mobileSearchForm.submit();
-  })
-})
